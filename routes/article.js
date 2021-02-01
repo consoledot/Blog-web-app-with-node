a97f329ef5b96128f6c9fba87c351aae5b4a9117
consoledot <bimbooladele@outlook.com>
2021-02-01 19:47:24 +0100

    already added database need some few updates

--- routes/article.js
+++ routes/article.js
@@ -1,12 +1,41 @@
 const express = require("express")
+const Article = require("../models/article")
 const router = express.Router()
 
+router.get("/", (req, res) => {
+    res.send('welcome')
+})
+
 router.get("/new",(req,res)=>{
-    res.render("articles/new")
+    res.render("articles/new", {article:{}})
+})
+router.get("/:id", async (req,res)=>{
+    try{
+        const article = await Article.findById(req.params.id)
+        res.render("articles/show",{article:article})
+    }
+    catch(e){
+        res.redirect("/")
+    }
+   
+    // if(article == null) 
+   
 })
 
-router.post("/",(req,res)=>{
+router.post("/", async (req,res)=>{
+    let article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+    })
+    try{ 
+        article = await article.save()
+        res.redirect(`/articles/${article.id}`)
+    }catch(e){
+        console.log(e)
+        res.render("articles/new", {article:article})
+    }
 
 })
 
-module.exports = router    
+module.exports = router 
--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const articleRouter = require("./routes/article")
+const Article = require("./models/article")
 const app = express()
 
+mongoose.connect("mongodb://localhost:27017/blog",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
 app.set("view engine", "ejs")
+app.use(express.urlencoded({extended:false}))
 
-app.use("/articles",articleRouter)
-
-app.get("/",(req,res)=>{
-    const articles =[{
-        title:"Gbas Gboas",
-        createdAt: new Date(),
-        description:"the only guy that knows nothing"
-    },
-    {
-        title:"Second is false",
-        createdAt: new Date(),
-        description:"levevu, nah why i no sabi French"
-    }]
+app.get("/", async (req,res)=>{
+    const articles = await Article.find().sort({createdAt: "desc"})
     res.render("articles/index", {articles:articles})
 })
+
+app.use("/articles", articleRouter)
+
 app.listen(5000,()=>{
     console.log("Running on port 5000")
-})
+})  
