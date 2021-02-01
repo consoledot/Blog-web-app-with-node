0e72a108a692b14dbf5f9d1f6c4c38b7f87d98ce
consoledot <bimbooladele@outlook.com>
2021-02-01 11:51:57 +0100

    added the input form page

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ app.set("view engine", "ejs")
 app.use("/articles",articleRouter)
 
 app.get("/",(req,res)=>{
-    res.render("index")
+    const articles =[{
+        title:"Gbas Gboas",
+        createdAt: new Date(),
+        description:"the only guy that knows nothing"
+    },
+    {
+        title:"Second is false",
+        createdAt: new Date(),
+        description:"levevu, nah why i no sabi French"
+    }]
+    res.render("articles/index", {articles:articles})
 })
 app.listen(5000,()=>{
     console.log("Running on port 5000")
